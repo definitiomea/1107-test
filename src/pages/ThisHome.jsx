761d6c534fd0e6d6cd38b1253c95e6c0abbc90be
Johnny_P <shinezl39@gmail.com>
2022-11-07 09:32:55 +0900

    페이지와 라우터 준비

---
+++ src/pages/ThisHome.jsx
@@ -0,0 +1,10 @@
+const ThisHome = () => {
+    return (
+        <>
+            <h1>홈 화면에 오신 것을 환영합니다</h1>
+            <p>아래를 통해 방명록을 작성하실 수 있습니다.</p>
+        </>
+    );
+}
+
+export default ThisHome;
