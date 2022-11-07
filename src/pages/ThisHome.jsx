4a459f7b00b8f526379c34e4c43890de9478b2f5
Johnny_P <shinezl39@gmail.com>
2022-11-07 09:41:53 +0900

    홈페이지 작성해보기

--- src/pages/ThisHome.jsx
+++ src/pages/ThisHome.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 const ThisHome = () => {
     return (
         <>
             <h1>홈 화면에 오신 것을 환영합니다</h1>
             <p>아래를 통해 방명록을 작성하실 수 있습니다.</p>
+            <p><Link to='/guestbook'>방명록 작성하기</Link></p>
         </>
     );
 }
