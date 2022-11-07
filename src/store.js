4f12e288fc3642c7c7814c9e2c3fc108b64a1f8d
Johnny_P <shinezl39@gmail.com>
2022-11-07 10:08:02 +0900

    리덕스 툴킷 설치

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+/* redux에서 createStore와 동일한 역할
++ 모듈이 따로 있다면 combine을 통해 묶어줘야 하지만, 툴킷에서는 기능이 통합된다. */
+
+export default configureStore({
+    reducer: {
+        
+    }
+})
